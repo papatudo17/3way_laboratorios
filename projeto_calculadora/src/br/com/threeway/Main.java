42a2a7c4ae421e8691b987af33727f07a39fac27
javasabmat <javasabmat@GYNLAB-JED10>
2017-10-19 21:12:54 -0200

    inicio do projeto com git ignore

---
+++ projeto_calculadora/src/br/com/threeway/Main.java
@@ -0,0 +1,14 @@
+package br.com.threeway;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+                   System.out.println("testando a calculadora");
+                   
+              
+                  
+                 
+	}
+
+}
